84d6e00f4ff0e1f5af6d9713033cd7e96e494865
Harri Ohra-aho <hoek@iki.fi>
2014-03-26 21:25:05 +0200

    Maybe using one connection works

--- app3.js
+++ app3.js
@@ -1,68 +1,57 @@
 var btSerial = require('bluetooth-serial-port');
 
-var scanner = new btSerial.BluetoothSerialPort();
 var addrs = ["00:13:12:31:25:81", "00:13:12:31:21:65"];
-var ports = [];
+var curr = 0;
+var port = new btSerial.BluetoothSerialPort();
 
-addrs.forEach(function(addr) {
-  var port = new btSerial.BluetoothSerialPort();
-  ports.push({ addr: addr, port: port, lastSeen: new Date().getTime() });
-});
-
-//scanner.on('found', function(address, name) {
-//  console.log("Found device: " + address + " " + name);
-//});
-
-var connectPort = function(port) {
-  port.port.findSerialPortChannel(port.addr, function(channel) {
-    console.log("connecting to " + port.addr + " " + channel);
-    port.port.connect(port.addr, channel, function() {
-      console.log('success ' + port.addr + " " + channel);
+var connectPort = function(addr) {
+  port.findSerialPortChannel(addr, function(channel) {
+    console.log("connecting to " + addr + " " + channel);
+    port.connect(addr, channel, function() {
+      console.log('success ' + addr + " " + channel);
       var data = "";
 
-      port.port.write(new Buffer('1', 'utf-8'), function(err, bytesWritten) {
+      port.write(new Buffer('1', 'utf-8'), function(err, bytesWritten) {
         if (err) console.log(err);
       });
 
-      port.port.on('data', function(buffer) {
+      port.on('data', function(buffer) {
         data += buffer.toString('utf-8');
         var parts = data.split("\n");
         data = parts.pop();
         parts.forEach(function(part, i, array) {
           console.log("data: " + part);
-          closePort(port.port);
+          closePort();
         });
-        port.lastSeen = new Date().getTime();
       });
     }, function() {
-      console.log("cannot connect " + port.addr);
+      console.log("cannot connect " + addr);
     });
   });
 };
 
-var closePort = function(port) {
+var closePort = function() {
   if (port.isOpen()) {
     port.close();
   }
 };
 
-var checkPorts = function() {
+var checkPort = function() {
   console.log("Fetch data");
-  var now = new Date().getTime();
-  ports.forEach(function(port) {
-    console.log("Query " + port.addr);
-    connectPort(port);
-  });
+  if (curr > 1) {
+    curr = 0;
+  }
+  var addr = addrs[curr];
+  console.log("Query " + addr);
+  connectPort(addr);
 };
 
 console.log("Start schedule");
-setInterval(checkPorts, 30000);
+setInterval(checkPort, 30000);
 
 process.on('SIGINT', function() {
   console.log("Shutting down...");
-  ports.forEach(function(port) {
-    closePort(port.port);
-  });
+  closePort();
   process.exit();
 });
 
